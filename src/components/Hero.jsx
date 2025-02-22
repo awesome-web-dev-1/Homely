9dd0ac9569390ffa3c3ac9891e4c92b8d14b372f
awesome web <166837590+awesome-web-dev-1@users.noreply.github.com>
2025-02-21 22:50:01 -0800

    some improvents

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section className='hero bg-[url("./images/hero-banner.png")]'>
+    <section className='hero bg-[url("/images/hero-banner.png")]'>
         <div className="container text-neutral-100">
             <h1 className="leading-[1.2]">Find the <br /> property That <br /> Feels Link Home.</h1>
             <p className="text-neutral-100/80 max-w-[440px] pt-[19px] pb-[30px]">
